804e9be9643ff16a48ade3ccb88d9b1b885fbe4f
Guillermo Galvan <ggalv@softserveinc.com>
2026-04-22 06:23:19 -0600

    feat: change message

--- HolaMicroservicio/Program.cs
+++ HolaMicroservicio/Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello Open Days Students!");
+app.MapGet("/", () => "Hello Open Days!");
 app.MapPost("/saludo", (SaludoRequest request) =>
 {
     var nombre = string.IsNullOrWhiteSpace(request.Nombre) ? "invitado" : request.Nombre.Trim();
